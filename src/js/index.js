0840e59a9e400c9838a2d0f11ea07969e0788695
MartinGO11 <ogutierrez.m94@gmail.com>
2020-12-12 15:49:35 +0000

    simplecounter

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,50 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
+import "@fortawesome/fontawesome-free";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="Calendar">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="Hours">{props.digithours % 10}</div>
+			<div className="Minutes">{props.digitminutes % 10}</div>
+			<div className="Seconds">{props.digitseconds % 10}</div>
+			<div className="Miliseconds">{props.digitmiliseconds % 10}</div>
+		</div>
+	);
+}
+SimpleCounter.propTypes = {
+	digithours: PropTypes.number,
+	digitminutes: PropTypes.number,
+	digitseconds: PropTypes.number,
+	digitmiliseconds: PropTypes.number
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function() {
+	const Hours = Math.floor(counter / 1000);
+	const Minutes = Math.floor(counter / 100);
+	const Seconds = Math.floor(counter / 10);
+	const Miliseconds = Math.floor(counter / 1);
+	console.log(Hours, Minutes, Seconds, Miliseconds);
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			digithours={Hours}
+			digitminutes={Minutes}
+			digitseconds={Seconds}
+			digitmiliseconds={Miliseconds}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
